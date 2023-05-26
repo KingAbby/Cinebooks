0f393a088b923dc394680044ae42cabe261cc33d
KingAbby <110515353+KingAbby@users.noreply.github.com>
2023-05-26 20:34:40 +0700

    Fix AboutUs

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -5,8 +5,8 @@ import Abi from "../assets/Images/Abi.jpg";
 import Fikri from "../assets/Images/Fikri.jpg";
 import Anthonio from "../assets/Images/Anthonio.jpg";
 import Gilbert from "../assets/Images/Gilbert.jpg";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faInstagram, faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faGithub, faInstagram, faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 function AboutUs() {
   const [flippedCard, setFlippedCard] = useState(null);
@@ -40,16 +40,29 @@ function AboutUs() {
             />
             <div className="card-body">
               <h5 className="card-title">
-                Abi Andrea Nurpasha (00000079207) - Informatika - 'KingAbby'
+                Abi Andrea Nurpasha (00000079207) - Informatika
               </h5>
 
               <p className="card-text">
                 <div className="icons">
-                  <a className="github-icon" href="https://github.com/KingAbby"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
+                  <a className="github-icon" href="https://github.com/KingAbby">
+                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                  </a>
 
-                  <a className="instagram-icon" href="https://www.instagram.com/abiandrea_/"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                  <a
+                    className="instagram-icon"
+                    href="https://www.instagram.com/abiandrea_/"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} size="lg" />
+                  </a>
 
-                  <a className="spotify-icon" href="https://open.spotify.com/user/21ctzxad6xictyjhrjqzse5cy?si=fc243f15b8a04cc5"> <FontAwesomeIcon icon={faSpotify} size="lg" /> </a>
+                  <a
+                    className="spotify-icon"
+                    href="https://open.spotify.com/user/21ctzxad6xictyjhrjqzse5cy?si=fc243f15b8a04cc5"
+                  >
+                    {" "}
+                    <FontAwesomeIcon icon={faSpotify} size="lg" />{" "}
+                  </a>
                 </div>
               </p>
 
@@ -62,9 +75,13 @@ function AboutUs() {
             </div>
           </div>
           <div className="card-back">
-            <h3>Isi Deskripsi Sendiri</h3>
-            <h5>Informatika</h5>
-            
+            <h4>
+              Saya memiliki selera film yang cenderung tertarik pada genre
+              "Action". Beberapa contoh film yang saya rekomendasikan yaitu
+              "John Wick", "Fast X", dan "Avatar". Saya menikmati ketegangan dan
+              kegembiraan yang ditawarkan oleh film-film Action ini dan senang
+              menjelajahi dunia aksi yang berbeda.
+            </h4>
             <button
               className="btn btn-primary custom-btn"
               onClick={() => toggleFlip(0)}
@@ -88,19 +105,35 @@ function AboutUs() {
             />
             <div className="card-body">
               <h5 className="card-title">
-                Fikri Naufal Andrasito (00000079229) - Informatika - 'naufaland'
+                Fikri Naufal Andrasito (00000079229) - Informatika
               </h5>
 
               <p className="card-text">
                 <div className="icons">
-                  <a className="github-icon" href="https://github.com/naufaland"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
+                  <a
+                    className="github-icon"
+                    href="https://github.com/naufaland"
+                  >
+                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                  </a>
 
-                  <a className="instagram-icon" href="https://www.instagram.com/fikrinaufaland_/"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                  <a
+                    className="instagram-icon"
+                    href="https://www.instagram.com/fikrinaufaland_/"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} size="lg" />
+                  </a>
 
-                  <a className="spotify-icon" href="https://open.spotify.com/user/tjfsbertyp0s4wzi6ypbrikui?si=387e79adb596420c"> <FontAwesomeIcon icon={faSpotify} size="lg" /> </a>
+                  <a
+                    className="spotify-icon"
+                    href="https://open.spotify.com/user/tjfsbertyp0s4wzi6ypbrikui?si=387e79adb596420c"
+                  >
+                    {" "}
+                    <FontAwesomeIcon icon={faSpotify} size="lg" />{" "}
+                  </a>
                 </div>
               </p>
-              
+
               <button
                 className="btn btn-primary custom-btn"
                 onClick={() => toggleFlip(1)}
@@ -110,9 +143,13 @@ function AboutUs() {
             </div>
           </div>
           <div className="card-back">
-            <h3>Isi Deskripsi Sendiri</h3>
-            <h5>Informatika</h5>
-            
+            <h4>
+              "Ngomongin soal Film, saya biasanya suka dengan Film yang berbau
+              action dan adventure. Mungkin 1 Film yang visa di jadiin
+              rekomendasi adalah 'Extarction' yang diperanin sama Chris
+              Hemsworth. Atau mau yang dicampur sama comedy nya? 'The Big 4' sih
+              rekomendasi saya".
+            </h4>
             <button
               className="btn btn-primary custom-btn"
               onClick={() => toggleFlip(1)}
@@ -129,23 +166,39 @@ function AboutUs() {
           onMouseLeave={() => toggleFlip(2)}
         >
           <div className="card-front">
-           <img
+            <img
               src={Anthonio}
               className="card-img-top custom-img"
               alt="Anthonio Raphael Purteuro Bulo"
             />
             <div className="card-body">
               <h5 className="card-title">
-                Anthonio Raphael Porteuro Bulo (00000079343) - Informatika - 'Dreamybull'
+                Anthonio Raphael Porteuro Bulo (00000079343) - Informatika
               </h5>
 
               <p className="card-text">
                 <div className="icons">
-                  <a className="github-icon" href="https://github.com/AnthonioMasbroo"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
+                  <a
+                    className="github-icon"
+                    href="https://github.com/AnthonioMasbroo"
+                  >
+                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                  </a>
 
-                  <a className="instagram-icon" href="https://www.instagram.com/fikrinaufaland_/"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                  <a
+                    className="instagram-icon"
+                    href="https://www.instagram.com/fikrinaufaland_/"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} size="lg" />
+                  </a>
 
-                  <a className="spotify-icon" href="https://open.spotify.com/user/tjfsbertyp0s4wzi6ypbrikui?si=387e79adb596420c"> <FontAwesomeIcon icon={faSpotify} size="lg" /> </a>
+                  <a
+                    className="spotify-icon"
+                    href="https://open.spotify.com/user/tjfsbertyp0s4wzi6ypbrikui?si=387e79adb596420c"
+                  >
+                    {" "}
+                    <FontAwesomeIcon icon={faSpotify} size="lg" />{" "}
+                  </a>
                 </div>
               </p>
 
@@ -158,9 +211,12 @@ function AboutUs() {
             </div>
           </div>
           <div className="card-back">
-            <h3>Isi Deskripsi Sendiri</h3>
-            <h5>Informatika</h5>
-            
+            <h4>
+              "Berbeda dari teman-teman saya, saya lebih suka film dengan genre
+              action dan comedy. 'Red Notice' adalah salah satu film yang saya
+              rekomendasikan untuk kalian, film ini dibintangi oleh Gal Gadot,
+              Ryan Reynolds, dan juga Dwayne Johnson"
+            </h4>
             <button
               className="btn btn-primary custom-btn"
               onClick={() => toggleFlip(2)}
@@ -177,23 +233,36 @@ function AboutUs() {
           onMouseLeave={() => toggleFlip(3)}
         >
           <div className="card-front">
-          <img
+            <img
               src={Gilbert}
               className="card-img-top custom-img"
               alt="Gilbert Parluhutan Pakpahan"
             />
             <div className="card-body">
               <h5 className="card-title">
-                Gilbert Parluhutan Pakpahan (00000079904) - Informatika - 'Giltp'
+                Gilbert Parluhutan Pakpahan (00000079904) - Informatika
               </h5>
 
               <p className="card-text">
                 <div className="icons">
-                  <a className="github-icon" href="https://github.com/Giltp"><FontAwesomeIcon icon={faGithub} size="lg" /></a>
+                  <a className="github-icon" href="https://github.com/Giltp">
+                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                  </a>
 
-                  <a className="instagram-icon" href="https://www.instagram.com/giltpp"><FontAwesomeIcon icon={faInstagram} size="lg" /></a>
+                  <a
+                    className="instagram-icon"
+                    href="https://www.instagram.com/giltpp"
+                  >
+                    <FontAwesomeIcon icon={faInstagram} size="lg" />
+                  </a>
 
-                  <a className="spotify-icon" href="https://open.spotify.com/user/e72d0ofcsfowu8zf1t3vwqbci"> <FontAwesomeIcon icon={faSpotify} size="lg" /> </a>
+                  <a
+                    className="spotify-icon"
+                    href="https://open.spotify.com/user/e72d0ofcsfowu8zf1t3vwqbci"
+                  >
+                    {" "}
+                    <FontAwesomeIcon icon={faSpotify} size="lg" />{" "}
+                  </a>
                 </div>
               </p>
 
@@ -208,7 +277,7 @@ function AboutUs() {
           <div className="card-back">
             <h3>Isi Deskripsi Sendiri</h3>
             <h5>Informatika</h5>
-            
+
             <button
               className="btn btn-primary custom-btn"
               onClick={() => toggleFlip(3)}
